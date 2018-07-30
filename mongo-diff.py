f6a47177490936a0bdf6cf4f87f702061430bc16
Logan Lembke <logan@blackhillsinfosec.com>
2018-07-30 10:39:48 -0600

    Retrieve a full report on the differences between the databases instead of exiting early

--- mongo-diff.py
+++ mongo-diff.py
@@ -39,13 +39,13 @@ def select_best_index(coll):
     for key, value in index_information.items():
         if len(value["key"]) > 1:
             toDelete.append(key)
-   
-    # get rid of ObjectID  
-    for key, value in index_information.items(): 
+
+    # get rid of ObjectID
+    for key, value in index_information.items():
         field_name = value["key"][0][0]
         if field_name in sample_item and isinstance(sample_item[field_name], bson.objectid.ObjectId):
             toDelete.append(key)
-            
+
     for key in toDelete:
         del index_information[key]
 
@@ -62,7 +62,7 @@ def select_best_index(coll):
     for index in index_information.values():
         if "unique" in index:
             return index["key"][0][0]
-    
+
     best_index = None
     best_index_count = 0
     for index in index_information.values():
@@ -121,49 +121,78 @@ def main():
         print("Databases contain different collections")
         return 1
 
+
+    collection_match = {}
+
     for coll in sorted(db1_coll_names)[::-1]:
         print("Comparing collection: {0}".format(coll))
-        
+        errors = [] # List of errors
+        match_count = 0 # How many records in DB1 match DB2
+        collection_counts = (0, 0) # How many records are in DB1.coll and DB2.coll
+
+        skip_matching = False # If there's no data or missing indexes
+
         db1_indexes = db1[coll].index_information()
         db2_indexes = db2[coll].index_information()
         if not all(x in db2_indexes.keys() for x in db1_indexes.keys()):
-            print("Collection {0} contains differing indexes".format(coll))
-            return 1
-        
-        db1_count = db1[coll].count()
-        db2_count = db2[coll].count()
-        if db1_count != db2_count:
-            print("Collection {0} contains a different amount of records in each database".format(coll))
-            return 1
-        
-        if db1_count == 0:
-            continue
+            errors += ["{0}.{1} and {2}.{3} contain differing indexes".format(DB1, coll, DB2, coll)]
+            skip_matching = True
 
-        best_field = select_best_index(db1[coll])
+        collection_counts = (db1[coll].count(), db2[coll].count())
 
-        print("\tSelecting field {0} for search".format(best_field))
-        for db1_entry in db1[coll].find():
-            db1_best_field_value = db1_entry[best_field]
-            found = False
-            
-            try:
-                for db2_entry in db2[coll].find({best_field: db1_best_field_value}):
-                    found = compare_entries(db1_entry, db2_entry)
-                    if found:
-                        break
+        if collection_counts[0] != collection_counts[1]:
+            errors += ["{0}.{1} contains {2} records while {3}.{4} contains {5} records".format(DB1, coll, collection_counts[0], DB2, coll, collection_counts[1])]
 
-            except bson.errors.InvalidBSON:
-                print("\tUNICODE ERROR. CONTINUING")
-                continue
-            
-            if not found:
-                print("Entry in {0} did not match an entry in {1}\n{2}".format(DB1, DB2, db1_entry))
-                return 1
+        if collection_counts[0] == 0:
+            print("\t{0}.{1} is empty".format(DB1, coll))
+            skip_matching = True
 
-    print("Databases are the same")
+        if not skip_matching:
+            best_field = select_best_index(db2[coll])
+
+            print("\tSelecting field {0} for searching {1}.{2}".format(best_field, DB2, coll))
+
+            percent_update_interval = 0.05
+            last_percent_update = -percent_update_interval
+            search_count = 0
+            for db1_entry in db1[coll].find():
+                db1_best_field_value = db1_entry[best_field]
+                found = False
+                try:
+                    for db2_entry in db2[coll].find({best_field: db1_best_field_value}):
+                        found = compare_entries(db1_entry, db2_entry)
+                        if found:
+                            match_count += 1
+                            break
+
+                except bson.errors.InvalidBSON:
+                    errors += ["Unicode error while iterating over {0}.{1}".format(DB1, coll)]
+
+                if not found:
+                    errors += ["Record in {0}.{1} did not match any record in {2}.{3}\n{4}".format(DB1, coll, DB2, coll, db1_entry)]
+
+                search_count += 1
+                search_percent = search_count / collection_counts[0]
+                if search_percent - last_percent_update >= percent_update_interval:
+                    print("\t{0:.0f}% finished comparing {1}.{2} to {3}.{4}".format(search_percent * 100, DB1, coll, DB2, coll))
+                    last_percent_update = search_percent
+
+        print("\t{0:.0f}% finished comparing {1}.{2} to {3}.{4}".format(100, DB1, coll, DB2, coll))
+        print("\tMatched {0} records out of {1} from {2}.{3} to records in {4}.{5}".format(match_count, collection_counts[0], DB1, coll, DB2, coll))
+        for err in errors:
+            print("\t" + err)
+
+        if len(errors) != 0:
+            collection_match[coll] = False
+        else:
+            collection_match[coll] = True
+
+    if not all(collection_match.values()):
+        return 1
+
+    print("Databases match")
     return 0
 
 
 if __name__ == "__main__":
     sys.exit(main())
-
