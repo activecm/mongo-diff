ae32507a8335b417ffa19d5e3eee36b1a2f935ed
Logan Lembke <logan@blackhillsinfosec.com>
2018-08-01 16:27:17 -0600

    Support multi-key indexes, improve index selection when indexes do not match across databases

--- README.md
+++ README.md
@@ -2,12 +2,18 @@
 
 MongoDiff checks whether two MongoDB databases are the same (including indexes).
 
-NOTE: The `_id` field is ignored since it is assumed to be unique.
+NOTE: All ObjectId fields are ignored since they are assumed to be unique to each database.
 
 # Usage
-`mongo-diff.py <uri> <database 1> <database 2>`
+`mongo-diff.py <uri> <database 1> <database 2> 2>unmatched-records`
 
 MongoDiff exits with status 0 if the databases are the same and 1 if they are not.
+
+Stdout displays a human readable status information and a report on
+how many records matched in each collection.
+
+Stderr displays a line separated list of records in `<database 1>`
+which were not found in `<database 2>`
 
 # Dependencies
 
@@ -19,3 +25,5 @@ MongoDiff exits with status 0 if the databases are the same and 1 if they are no
 For best performance, ensure a unique index exists in each collection for the dataset being compared. If a unique index does not exist, MongoDiff will attempt to find the index with the most unique values.
 
 If no index exists other than \_id, performance will degrade to O(n^2)
+
+MongoDiff will refuse to use any indexes which contain a field with type ObjectId.
--- mongo-diff.py
+++ mongo-diff.py
@@ -6,12 +6,23 @@ import bson
 
 NO_ARRAY_ORDER = False
 
-def count_unique_values(field_name, coll):
+def count_unique_values(field_names, coll):
     #abominations to get mongo to count how many distinct values
     #exist for a field
+    pipeline = []
+    project_inner = {}
+    for name in field_names:
+        project_inner[name] = 1
+    project_outer = {"$project": project_inner}
+    group_inner = {}
+    for name in field_names:
+        group_inner[name] = "$" + name
+    group_outer = {"$group": {"_id": group_inner}}
     pipeline = [
-        {"$project": { field_name: 1 } },
-        {"$group": { "_id": "$" + field_name } },
+        project_outer,
+        group_outer,
+        #{"$project": { field_name: 1 } },
+        #{"$group": { "_id": "$" + field_name } },
         {"$project": { "dummy": "dummy" } },
         {"$group": { "_id": "dummy", "count": { "$sum": 1 } } }
     ]
@@ -21,57 +32,63 @@ def count_unique_values(field_name, coll):
     return results[0]["count"]
 
 
-def select_best_index(coll):
+def select_best_index(coll1, coll2):
     # get a sample from the collection
-    sample_item = list(coll.find(limit=1))
+    coll1_sample_item = list(coll1.find(limit=1))
+    coll2_sample_item = list(coll2.find(limit=1))
 
     # if there is no data in the collection, return None
-    if len(sample_item) == 0:
+    if len(coll1_sample_item) == 0 or len(coll2_sample_item) == 0:
         return None
 
-    sample_item = sample_item[0]
+    coll1_sample_item = coll1_sample_item[0]
+    coll2_sample_item = coll2_sample_item[0]
 
     # get a copy of the index info
-    index_information = coll.index_information()
+    coll1_index_information = coll1.index_information()
 
-    # get rid of multi key indexes
-    toDelete = []
-    for key, value in index_information.items():
-        if len(value["key"]) > 1:
-            toDelete.append(key)
+    # get rid of indexes that aren't shared by the two collections
+    to_delete = []
+    coll2_index_information = coll2.index_information()
+    for index_name, index_doc in coll1_index_information.items():
+        if index_name not in coll2_index_information:
+            to_delete.append(index_name)
 
-    # get rid of ObjectID
-    for key, value in index_information.items():
-        field_name = value["key"][0][0]
-        if field_name in sample_item and isinstance(sample_item[field_name], bson.objectid.ObjectId):
-            toDelete.append(key)
+    # get rid of ObjectID based indexes
+    for index_name, index_doc in coll1_index_information.items():
+        for field_name, index_type in index_doc["key"]:
+            if (
+                field_name in coll1_sample_item and isinstance(coll1_sample_item[field_name], bson.objectid.ObjectId) or
+                field_name in coll2_sample_item and isinstance(coll2_sample_item[field_name], bson.objectid.ObjectId)
+            ):
+                to_delete.append(index_name)
 
-    for key in toDelete:
-        del index_information[key]
+    for index_name in to_delete:
+        del coll1_index_information[index_name]
 
     # no candidate indexes
-    if len(index_information) == 0:
+    if len(coll1_index_information) == 0:
         # just choose the first non ObjectID field
-        for key, value in sample_item:
-            if not isinstance(value, bson.objectid.ObjectID):
-                return key
-        # no non ObjectID fields....
+        for key, value in coll1_sample_item.items():
+            if key in coll2_sample_item and not isinstance(value, bson.objectid.ObjectId):
+                return [key]
+        # no shared non ObjectID fields....
         return None
 
-    # search for a unique index
-    for index in index_information.values():
-        if "unique" in index:
-            return index["key"][0][0]
+    for index_name, index_doc in coll1_index_information.items():
+        if "unique" in index_doc and index_doc["unique"]:
+            return [x[0] for x in index_doc["key"]]
 
     best_index = None
     best_index_count = 0
-    for index in index_information.values():
-        field_name = index["key"][0][0]
-        index_count = count_unique_values(field_name, coll)
+    for index_name, index_doc in coll1_index_information.items():
+        index_keys = [x[0] for x in index_doc["key"]]
+        coll1_index_count = count_unique_values(index_keys, coll1)
+        coll2_index_count = count_unique_values(index_keys, coll2)
+        index_count = min(coll1_index_count, coll2_index_count)
         if index_count > best_index_count:
-            best_index = field_name
+            best_index = index_keys
             best_index_count = index_count
-
     return best_index
 
 
@@ -83,6 +100,7 @@ def compare_entries(db1_entry, db2_entry):
     for key, db1_value in db1_entry.items():
         if isinstance(db1_value, bson.objectid.ObjectId):
             continue
+
         db2_value = db2_entry[key]
         if isinstance(db2_value, float):
             same = abs(db1_value - db2_value) < 0.01
@@ -148,18 +166,18 @@ def main():
             skip_matching = True
 
         if not skip_matching:
-            best_field = select_best_index(db2[coll])
+            best_fields = select_best_index(db1[coll], db2[coll])
 
-            print("\tSelecting field {0} for searching {1}.{2}".format(best_field, DB2, coll))
+            print("\tSelecting fields {0} for searching {1}.{2}".format(best_fields, DB2, coll))
 
             percent_update_interval = 0.05
             last_percent_update = -percent_update_interval
             search_count = 0
             for db1_entry in db1[coll].find():
-                db1_best_field_value = db1_entry[best_field]
+                best_fields_selector = {best_field: db1_entry[best_field] for best_field in best_fields}
                 found = False
                 try:
-                    for db2_entry in db2[coll].find({best_field: db1_best_field_value}):
+                    for db2_entry in db2[coll].find(best_fields_selector):
                         found = compare_entries(db1_entry, db2_entry)
                         if found:
                             match_count += 1
@@ -178,9 +196,14 @@ def main():
                     last_percent_update = search_percent
 
         print("\t{0:.0f}% finished comparing {1}.{2} to {3}.{4}".format(100, DB1, coll, DB2, coll))
-        print("\tMatched {0} records out of {1} from {2}.{3} to records in {4}.{5}".format(match_count, collection_counts[0], DB1, coll, DB2, coll))
+        print(
+            "\tMatched {0} records out of {1} ({2:.2f}%) from {3}.{4} to records in {5}.{6}".format(
+                match_count, collection_counts[0], 100 * match_count / collection_counts[0],
+                DB1, coll, DB2, coll
+            )
+        )
         for err in errors:
-            print("\t" + err)
+            print(err, file=sys.stderr)
 
         if len(errors) != 0:
             collection_match[coll] = False
